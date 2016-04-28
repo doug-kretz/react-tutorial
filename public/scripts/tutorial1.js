8c26820dc6363bbc8f7ab8bde6cf8ad227db60d6
Doug Kretz <richard.kretz@tealiulm.com>
2016-04-28 09:39:05 -0700

    Load data from server

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -4,11 +4,34 @@ var data = [
 ]
 
 var CommentBox = React.createClass({
+	getInitialState: function() {
+		return {data: []};
+	},
+
+	loadCommentsFromServer: function(){
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+
+	componentDidMount: function(){
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
+
 	render: function() {
 		return (
 			<div className="commentBox">
 				<h1>Comments</h1>
-				<CommentList data={this.props.data} />
+				<CommentList data={this.state.data} />
 				<CommentForm />
 			</div>
 		);
@@ -64,6 +87,6 @@ var Comment = React.createClass({
 });
 
 ReactDOM.render(
-	<CommentBox data={data} />,
+	<CommentBox url="/api/comments" pollInterval={2000}/>,
 	document.getElementById('content')
 );
